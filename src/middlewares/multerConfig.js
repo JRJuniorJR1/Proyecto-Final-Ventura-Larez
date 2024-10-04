a6f8be860aa0274c7f8681910732d2d7b15f9e4d
Ventura Larez <venturalarez132@gmail.com>
2024-10-03 23:18:33 -0300

    parchando carpeta public

--- src/middlewares/multerConfig.js
+++ src/middlewares/multerConfig.js
@@ -1,25 +1,29 @@
 import multer from 'multer';
 import __dirname from '../../utils.js';
 import path from 'path';
+import fs from 'fs';
 
-
-//configuracion de donde se guardaran los archivos:
+// Configuración de dónde se guardarán los archivos:
 const storage = multer.diskStorage({
-  //destination hace referencia a la carpeta donde se guardara el archivo.
   destination: function (req, file, cb) {
-    let path = __dirname + '/src/public/documents'; // Especificamos la ruta; Por defecto, guarda en 'documents'
-    if (file.fieldname === 'profile') path = __dirname + '/src/public/profiles';
-    if (file.fieldname === 'product') path = __dirname + '/src/public/products';
-    cb(null, path);
+    let folderPath = __dirname + '/src/public/documents'; // Carpeta predeterminada
+    if (file.fieldname === 'profile') folderPath = __dirname + '/src/public/profiles';
+    if (file.fieldname === 'product') folderPath = __dirname + '/src/public/products';
+
+    // Verifica si la carpeta existe, si no, la crea
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+
+    cb(null, folderPath);
   },
-  //filename hace referencia al nombre final que tendra el archivo
   filename: function (req, file, cb) {
-    console.log(file)
-    cb(null, `${Date.now()}-${file.originalname.replace(/\s/g, '')}`); //elimina los espacios en blanco
+    console.log(file);
+    cb(null, `${Date.now()}-${file.originalname.replace(/\s/g, '')}`); // Elimina los espacios en blanco
   }
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 export default upload;
 
 
@@ -42,4 +46,27 @@ export default upload;
 //   }
 // });
 
+//antiguo
+/* import multer from 'multer';
+import __dirname from '../../utils.js';
+import path from 'path';
 
+
+//configuracion de donde se guardaran los archivos:
+const storage = multer.diskStorage({
+  //destination hace referencia a la carpeta donde se guardara el archivo.
+  destination: function (req, file, cb) {
+    let path = __dirname + '/src/public/documents'; // Especificamos la ruta; Por defecto, guarda en 'documents'
+    if (file.fieldname === 'profile') path = __dirname + '/src/public/profiles';
+    if (file.fieldname === 'product') path = __dirname + '/src/public/products';
+    cb(null, path);
+  },
+  //filename hace referencia al nombre final que tendra el archivo
+  filename: function (req, file, cb) {
+    console.log(file)
+    cb(null, `${Date.now()}-${file.originalname.replace(/\s/g, '')}`); //elimina los espacios en blanco
+  }
+});
+
+const upload = multer({storage});
+export default upload; */
